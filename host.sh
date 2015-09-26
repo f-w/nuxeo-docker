2688b4e68e4936a08b94780394f0b672b9356282
f-w <root@v2.firstvoices.ca>
2015-09-26 17:23:26 -0500

    auto run docker container

--- host.sh
+++ host.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 docker build -t bcdevexchange/nuxeo .
-docker run -d -p 80:8080 bcdevexchange/nuxeo
+# auto start container, assuming host uses upstart
+cp docker-containers.conf /etc/init
+docker run -d --name=bcdevexchange-nuxeo -p 80:8080 bcdevexchange/nuxeo
